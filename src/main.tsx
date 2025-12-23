b05c241ac44b08a869a6f18fd147f0299cfa06c5
tom <t.bass0326@gmail.com>
2025-12-24 02:14:05 +0900

    Add basepath to router configuration using BASE_URL env variable

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+  basepath: import.meta.env.BASE_URL,
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
